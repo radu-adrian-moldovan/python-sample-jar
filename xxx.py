5b509afd52c8e6350ecd64e4defaa1e85d704781
Radu MOLDOVAN <radu.adrian.moldovan@gmail.com>
2021-03-25 09:22:02 +0200

    Update xxx.py

--- xxx.py
+++ xxx.py
@@ -2,6 +2,11 @@ import random
 from pyspark import SparkContext, SQLContext
 from pyspark.sql import Row
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
+from time import time
+import numpy as np
+from random import random
+from operator import add
+from pyspark.sql import SparkSession
 
 TEST = "xxx_test"
 
@@ -9,6 +14,33 @@ def my_print(x):
     print(x)
     
 print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> before <<<<<<<<<<<<<<<<<<<<<<<<<")
+
+
+spark = SparkSession.builder.appName('xxx CalculatePi').getOrCreate()
+sc = spark.sparkContext
+
+def is_point_inside_unit_circle(p):
+    # p is useless here
+    x, y = random(), random()
+    return 1 if x*x + y*y < 1 else 0
+
+
+t_0 = time()
+
+# parallelize creates a spark Resilient Distributed Dataset (RDD)
+# its values are useless in this case
+# but allows us to distribute our calculation (inside function)
+count = sc.parallelize(range(0, n)) \
+             .map(is_point_inside_unit_circle).reduce(add)
+print(np.round(time()-t_0, 3), "seconds elapsed for spark approach and n=", n)
+print("Pi is roughly %f" % (4.0 * count / n))
+
+# VERY important to stop SparkSession
+# Otherwise, the job will keep running indefinitely
+spark.stop()
+
+
+
 
 
 def sample_function(sc: SparkContext):
